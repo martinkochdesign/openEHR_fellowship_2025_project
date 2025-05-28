1c5324255db95595bf14702277f10a7ab86ce745
martinkochdesign <72647122+martinkochdesign@users.noreply.github.com>
2025-05-28 14:20:29 +0200

    test download of zipFile

--- automation_test.py
+++ automation_test.py
@@ -1,7 +1,23 @@
 import datetime
+import requests
 
 x = datetime.datetime.now()
 print(x)
 
 with open("dataset.js", "w") as f:
   f.write(str(x))
+
+
+URL = 'https://ckm.salut.gencat.cat/ckm/retrieveResources?resourceType=archetype&format=ADL&state1=INITIAL&state2=DRAFT&state3=TEAMREVIEW&state4=REVIEWSUSPENDED&state5=PUBLISHED&state6=REASSESS_DRAFT&state7=REASSESS_TEAMREVIEW&state8=REASSESS_REVIEWSUSPENDED'
+zipFileName = 'MyTestZipFile.zip'
+def download_url(url, zipFileName, chunk_size=128):
+	try:
+		r = requests.get(url, stream=True)
+		with open(zipFileName, 'wb') as fd:
+			for chunk in r.iter_content(chunk_size=chunk_size):
+				fd.write(chunk)
+	except OSError:
+		print('No connection to the server!')
+		return None
+
+download_url(URL, zipFileName)
